55c2d8dad1940fcfed24702dec04f8ba23a73620
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-01-26 19:58:06 -0500

    Create global.R

---
+++ global.R
@@ -0,0 +1,7 @@
+library(shiny)
+library(ggplot2)
+options(scipen = 999)
+options(shiny.maxRequestSize = 30 * 1024^2)
+
+Palette <- c("#1F78B4", "#33A02C", "#E31A1C", "#FF7F00", "#6A3D9A", "#B15928", "#A6CEE3", 
+             "#B2DF8A", "#FB9A99", "#FDBF6F", "#CAB2D6", "#FFFF99", "#999999")
